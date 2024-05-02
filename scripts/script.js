0b640ecfe7c94708bae24f3a51abfdb5ddca68b6
lukaNikolava <nikolavaluka475@gmail.com>
2024-05-02 13:38:02 +0400

    added delete button and  made code more readable

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,33 @@ let addNoteBtn = document.querySelector('#addNoteBtn');
 let title = document.querySelector('#title');
 let content = document.querySelector('#content');
 
+const createElementAndAppend = ( type , className , textContent , parent )=>{
+    let element = document.createElement(type);
+    element.className = className;
+    element.textContent = textContent;
+    parent.appendChild(element);
+    return element;
+}
+
 addNoteBtn.addEventListener('click' , ()=>{
     if(title.value && content.value){
 
         let newNote = document.createElement('div');
         newNote.className = 'noteCard';
 
-        let cardHeader = document.createElement('div');
-        cardHeader.className = 'cardHeader';
-        cardHeader.textContent = title.value;
-        newNote.appendChild(cardHeader);
-
-        let cardContent = document.createElement('div');
-        cardContent.className = 'cardContent';
-        cardContent.textContent = content.value;
-        newNote.appendChild(cardContent);
+        createElementAndAppend( 'div' , 'cardHeader' , title.value , newNote )
+        createElementAndAppend( 'div' , 'cardContent' , content.value , newNote )
+        
+        let deleteBtnContainer = createElementAndAppend('div', 'deleteBtnContainer', "", newNote);
+        let deleteBtn = createElementAndAppend('div', 'deleteBtn', "Delete", deleteBtnContainer);
 
         document.querySelector('.cardsSection').appendChild(newNote);
         title.value = '';
         content.value = '';
+
+        deleteBtn.addEventListener( 'click' , ()=>{
+            newNote.remove();
+        })
     }
 })
 
